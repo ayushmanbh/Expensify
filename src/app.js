44d563a826bd0770a83bc8cd801fa9cde20e3b91
ayushmanbh <ashbh.dhp14@gmail.com>
2020-06-11 11:30:04 +0530

    setup prod webpack build

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import 'react-dates/initialize'
 import 'react-dates/lib/css/_datepicker.css'
-import React from 'react'
-import ReactDOM from 'react-dom'
 import 'normalize-css/normalize.css';
 import './styles/styles.scss'
+import React from 'react'
+import ReactDOM from 'react-dom'
 import AppRouter from './routers/AppRouter'
 import { Provider } from 'react-redux';
 import { addExpense } from './actions/expenses';
@@ -12,13 +12,6 @@ import getVisibleExpenses from './selectors/expenses'
 import configureStore from './store/configureStore'
 
 const store = configureStore()
-
-store.dispatch(addExpense({ description: 'water bill', amount: 400 }))
-store.dispatch(addExpense({ description: 'gas bill', amount: 800, createdAt: 1000 }))
-store.dispatch(addExpense({ description: 'Rent', amount: 10050 }))
-
-const state = store.getState()
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
 
 const jsx = (
   <Provider store={store}>
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,52 @@
 //entry --> output
 const path = require('path')
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
-  },
-  mode: 'development'
-}
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = (env) => {
+  const inProduction = env === 'production'
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    plugins: [new MiniCssExtractPlugin({
+      filename: 'styles.css'
+    })],
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: true
+            }
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }]
+    },
+    devtool: inProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
+    },
+    mode: 'none'
+  }
+} 
