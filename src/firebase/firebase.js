6a654582854d346a42450a5a1860f67e416f7ada
ayushmanbh <ashbh.dhp14@gmail.com>
2020-06-16 12:08:09 +0530

    setup test env variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,31 +1,108 @@
 import * as firebase from 'firebase'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDnXTtLfnoCezLtFCZJW4fSP0uTBvy4vZ0",
-  authDomain: "expensify-23e69.firebaseapp.com",
-  databaseURL: "https://expensify-23e69.firebaseio.com",
-  projectId: "expensify-23e69",
-  storageBucket: "expensify-23e69.appspot.com",
-  messagingSenderId: "165085635536",
-  appId: "1:165085635536:web:6a679b45c8b3e6a2020fd5"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 }
 
 firebase.initializeApp(firebaseConfig)
 const db = firebase.database()
-db.ref().set({
-  name: 'Andrew mead',
-  age: 26,
-  isSingle: true,
-  location: {
-    city: 'india',
-    country: 'Asia'
-  }
-})
 
-db.ref('age').set(27)
-db.ref('location/city').set('Agra')
-db.ref('location/country').set('India')
-db.ref('attributes').set({
-  height: 175,
-  weight: 62
-})
+export { firebase, db as default }
+
+// db.ref('expenses').on('child_removed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val())
+// })
+
+// db.ref('expenses').on('value', (snapshot) => {
+//   const expenses = []
+//   snapshot.forEach((childSnapshot) => {
+//     expenses.push({
+//       id: childSnapshot.key,
+//       ...childSnapshot.val()
+//     })
+//   })
+//   console.log(expenses)
+// })
+
+// db.ref('expenses').once('value').then((snapshot) => {
+//   const expenses = []
+//   snapshot.forEach((childSnapshot) => {
+//     expenses.push({
+//       id: childSnapshot.key,
+//       ...childSnapshot.val()
+//     })
+//   })
+//   console.log(expenses)
+// })
+
+// db.ref('expenses').push({
+//   description: 'gas',
+//   note: '',
+//   amount: 109500,
+//   createdAt: 973737333
+// })
+
+// db.ref('expenses').push({
+//   description: 'rent',
+//   note: '',
+//   amount: 109870,
+//   createdAt: 9737363633
+// })
+
+// db.ref('location/city').
+//   once('value')
+//   .then((snapshot) => {
+//     const val = snapshot.val()
+//     console.log(val)
+//   })
+//   .catch((e) => {
+//     console.log(e)
+//   })
+
+// db.ref().on('value', (snapshot) => {
+//   const val = snapshot.val()
+//   console.log(`${val.name} is a ${val.job} at ${val.age}`)
+// }, (e) => {
+//   console.log('Error', e)
+// })
+
+// db.ref().set({
+//   name: 'Andrew mead',
+//   age: 26,
+//   isSingle: true,
+//   location: {
+//     city: 'Agra',
+//     country: 'India'
+//   }
+// }).then(() => {
+//   console.log('data is saved')
+// }).catch((e) => {
+//   console.log('save failed', e)
+// })
+
+// db.ref('attributes').set({
+//   height: 175,
+//   weight: 62
+// }).then(() => {
+//   console.log('data is updated')
+// }).catch((e) => {
+//   console.log('update failed', e)
+// })
+
+// db.ref().update({
+//   isSingle: null,
+//   job: 'Manager',
+//   'location/city': 'Boston'
+// })
+
+// db.ref().remove().then(() => {
+//   console.log('Removed')
+// }).catch((e) => {
+//   console.log('Error', e)
+// })
